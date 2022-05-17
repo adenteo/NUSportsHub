b683e4ef94d1d2ce4ce5e93f0ba2c39a628d06b3
Aden <adenteo@gmail.com>
2022-05-17 18:34:46 +0800

    Added createBottomTabNavigator to home screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 function HomeScreen() {
     return (
@@ -13,12 +14,32 @@ function HomeScreen() {
     );
 }
 
-function SettingsScreen() {
+function ExploreScreen() {
     return (
         <View
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
-            <Text>Settings!</Text>
+            <Text>Explore!</Text>
+        </View>
+    );
+}
+
+function BookingsScreen() {
+    return (
+        <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+            <Text>Bookings!</Text>
+        </View>
+    );
+}
+
+function ProfileScreen() {
+    return (
+        <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+            <Text>Profile!</Text>
         </View>
     );
 }
@@ -28,9 +49,65 @@ const Tab = createBottomTabNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
+            <Tab.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                    headerShown: false,
+                    tabBarStyle: { backgroundColor: "#0C3370" },
+                }}
+            >
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="home"
+                                color={"white"}
+                                size={30}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Explore"
+                    component={ExploreScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="earth"
+                                color={"white"}
+                                size={30}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Bookings"
+                    component={BookingsScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="book"
+                                color={"white"}
+                                size={30}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={ProfileScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="account"
+                                color={"white"}
+                                size={30}
+                            />
+                        ),
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     );
