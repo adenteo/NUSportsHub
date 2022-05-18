1eb7df5650f304c3a912d7af0bff7ca45fc9d13b
Aden <adenteo@gmail.com>
2022-05-18 14:11:55 +0800

    Added splash screen.
    Added SplashScreen from expo-splash-screen

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { useState, useEffect } from "react";
+import * as Font from "expo-font";
 
 function HomeScreen() {
     return (
         <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
         >
             <Text>Home!</Text>
         </View>
@@ -47,6 +54,33 @@ function ProfileScreen() {
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+                // Keep the splash screen visible while we fetch resources
+                await SplashScreen.preventAutoHideAsync();
+                // Pre-load fonts, make any API calls you need to do here
+                await Font.loadAsync({
+                    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+                });
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                // Tell the application to render
+                setAppIsReady(true);
+                await SplashScreen.hideAsync();
+            }
+        }
+
+        prepare();
+    }, []);
+
+    if (!appIsReady) {
+        return null;
+    }
+
     return (
         <NavigationContainer>
             <Tab.Navigator
@@ -54,16 +88,22 @@ export default function App() {
                 screenOptions={{
                     headerShown: false,
                     tabBarStyle: { backgroundColor: "#0C3370" },
+                    tabBarActiveTintColor: "black",
+                    tabBarInactiveTintColor: "white",
+                    tabBarLabelStyle: {
+                        fontFamily: "Montserrat-Bold",
+                        fontSize: 13,
+                    },
                 }}
             >
                 <Tab.Screen
                     name="Home"
                     component={HomeScreen}
                     options={{
-                        tabBarIcon: ({ color, size }) => (
+                        tabBarIcon: ({ focused, color, size }) => (
                             <MaterialCommunityIcons
                                 name="home"
-                                color={"white"}
+                                color={color}
                                 size={30}
                             />
                         ),
@@ -73,10 +113,10 @@ export default function App() {
                     name="Explore"
                     component={ExploreScreen}
                     options={{
-                        tabBarIcon: ({ color, size }) => (
+                        tabBarIcon: ({ color }) => (
                             <MaterialCommunityIcons
                                 name="earth"
-                                color={"white"}
+                                color={color}
                                 size={30}
                             />
                         ),
@@ -86,10 +126,10 @@ export default function App() {
                     name="Bookings"
                     component={BookingsScreen}
                     options={{
-                        tabBarIcon: ({ color, size }) => (
+                        tabBarIcon: ({ color }) => (
                             <MaterialCommunityIcons
                                 name="book"
-                                color={"white"}
+                                color={color}
                                 size={30}
                             />
                         ),
@@ -99,10 +139,10 @@ export default function App() {
                     name="Profile"
                     component={ProfileScreen}
                     options={{
-                        tabBarIcon: ({ color, size }) => (
+                        tabBarIcon: ({ color }) => (
                             <MaterialCommunityIcons
                                 name="account"
-                                color={"white"}
+                                color={color}
                                 size={30}
                             />
                         ),
